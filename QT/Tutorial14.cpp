8282ea0fb3179659d5f95a194faa281c1ae6465b
yemoxie <58982047+yemoxie@users.noreply.github.com>
2022-09-22 19:14:29 +0800

    Add files via upload

---
+++ QT/Tutorial14.cpp
@@ -0,0 +1,13 @@
+
+#include <QApplication>
+
+#include "gameboard.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    GameBoard board;
+    board.setGeometry(100, 100, 500, 355);
+    board.show();
+    return app.exec();
+}
